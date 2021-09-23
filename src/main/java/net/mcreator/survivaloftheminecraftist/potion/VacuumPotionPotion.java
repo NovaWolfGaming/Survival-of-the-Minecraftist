56e9fb50a645744c14edb1757402a38d1298637b
Laureed <Ludwi@192.168.0.96>
2021-09-23 23:25:37 +0200

    Added vacuum potion + effect

---
+++ src/main/java/net/mcreator/survivaloftheminecraftist/potion/VacuumPotionPotion.java
@@ -0,0 +1,26 @@
+
+package net.mcreator.survivaloftheminecraftist.potion;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.EffectInstance;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class VacuumPotionPotion {
+	@ObjectHolder("survival_of_the_minecraftist:vacuum_potion")
+	public static final Potion potionType = null;
+	@SubscribeEvent
+	public static void registerPotion(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		public PotionCustom() {
+			super(new EffectInstance(VacuumEffectPotionEffect.potion, 3600, 0, false, true));
+			setRegistryName("vacuum_potion");
+		}
+	}
+}
