1cc5d180869a00e3a112b999234a6b39c24eaf9f
NovaWolfGaming <souls@DESKTOP-0CQ9V5C.lan>
2021-09-23 12:35:34 -0700

    jgfjfjfjhfj

---
+++ src/main/java/net/mcreator/survivaloftheminecraftist/itemgroup/SOTMPlantsItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.survivaloftheminecraftist.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.survivaloftheminecraftist.block.BlazeFlowerBlock;
+import net.mcreator.survivaloftheminecraftist.SurvivalOfTheMinecraftistModElements;
+
+@SurvivalOfTheMinecraftistModElements.ModElement.Tag
+public class SOTMPlantsItemGroup extends SurvivalOfTheMinecraftistModElements.ModElement {
+	public SOTMPlantsItemGroup(SurvivalOfTheMinecraftistModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabsotm_plants") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(BlazeFlowerBlock.block);
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
