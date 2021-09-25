f7e955934b01fcd8f146b6293a4c58e8981d3106
Laureed <Ludwi@192.168.0.96>
2021-09-25 13:19:06 +0200

    Updated Chisel Texture

--- src/main/java/net/mcreator/survivaloftheminecraftist/item/ChiselItem.java
+++ src/main/java/net/mcreator/survivaloftheminecraftist/item/ChiselItem.java
@@ -1,24 +1,29 @@
 
 package net.mcreator.survivaloftheminecraftist.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.survivaloftheminecraftist.itemgroup.SOTMToolsAndWeaponsItemGroup;
+import net.mcreator.survivaloftheminecraftist.SurvivalOfTheMinecraftistModElements;
+
 @SurvivalOfTheMinecraftistModElements.ModElement.Tag
 public class ChiselItem extends SurvivalOfTheMinecraftistModElements.ModElement {
-
 	@ObjectHolder("survival_of_the_minecraftist:chisel")
 	public static final Item block = null;
-
 	public ChiselItem(SurvivalOfTheMinecraftistModElements instance) {
 		super(instance, 120);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(SOTMToolsAndWeaponsItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("chisel");
@@ -48,7 +53,5 @@ public class ChiselItem extends SurvivalOfTheMinecraftistModElements.ModElement 
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
