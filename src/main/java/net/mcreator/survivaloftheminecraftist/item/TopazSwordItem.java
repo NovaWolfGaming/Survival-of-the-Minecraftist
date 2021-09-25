70b2da2254f88a1b07b7bcf54231ab5874f7b69f
Laureed <Ludwi@192.168.0.96>
2021-09-25 13:10:00 +0200

    Added Topaz Sword + Recipe

---
+++ src/main/java/net/mcreator/survivaloftheminecraftist/item/TopazSwordItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.survivaloftheminecraftist.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.survivaloftheminecraftist.SurvivalOfTheMinecraftistModElements;
+
+@SurvivalOfTheMinecraftistModElements.ModElement.Tag
+public class TopazSwordItem extends SurvivalOfTheMinecraftistModElements.ModElement {
+	@ObjectHolder("survival_of_the_minecraftist:topaz_sword")
+	public static final Item block = null;
+	public TopazSwordItem(SurvivalOfTheMinecraftistModElements instance) {
+		super(instance, 121);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 4.5f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("topaz_sword"));
+	}
+}
