f122bd679ea503c203b973b8c0a0da3aa772056e
NovaWolfGaming <souls@DESKTOP-0CQ9V5C.lan>
2021-09-25 05:17:01 -0700

    Changed Recipe for Topaz Shears

--- src/main/java/net/mcreator/survivaloftheminecraftist/item/TopazShearItem.java
+++ src/main/java/net/mcreator/survivaloftheminecraftist/item/TopazShearItem.java
@@ -1,19 +1,27 @@
 
 package net.mcreator.survivaloftheminecraftist.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ShearsItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.survivaloftheminecraftist.SurvivalOfTheMinecraftistModElements;
+
 @SurvivalOfTheMinecraftistModElements.ModElement.Tag
 public class TopazShearItem extends SurvivalOfTheMinecraftistModElements.ModElement {
-
 	@ObjectHolder("survival_of_the_minecraftist:topaz_shear")
 	public static final Item block = null;
-
 	public TopazShearItem(SurvivalOfTheMinecraftistModElements instance) {
 		super(instance, 133);
 	}
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new ShearsItem(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(100)) {
+		elements.items.add(() -> new ShearsItem(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(470)) {
 			@Override
 			public int getItemEnchantability() {
 				return 16;
@@ -23,8 +31,6 @@ public class TopazShearItem extends SurvivalOfTheMinecraftistModElements.ModElem
 			public float getDestroySpeed(ItemStack stack, BlockState block) {
 				return 7f;
 			}
-
 		}.setRegistryName("topaz_shear"));
 	}
-
 }
