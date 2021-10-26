f1bdc2871192fec4e915bf05d37eb081f3cf2e3f
GalaxyPouriya <f.p@Galaxy-Pouriya>
2021-10-26 18:31:25 +0330

    Added Imond Ingot

---
+++ src/main/java/net/mcreator/survivaloftheminecraftist/item/ImondIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.survivaloftheminecraftist.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.survivaloftheminecraftist.SurvivalOfTheMinecraftistModElements;
+
+@SurvivalOfTheMinecraftistModElements.ModElement.Tag
+public class ImondIngotItem extends SurvivalOfTheMinecraftistModElements.ModElement {
+	@ObjectHolder("survival_of_the_minecraftist:imond_ingot")
+	public static final Item block = null;
+	public ImondIngotItem(SurvivalOfTheMinecraftistModElements instance) {
+		super(instance, 574);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("imond_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
